46e1f61ba311b99e683ea2c121fcb8943b50047f
JA-Richardson <arichardson1103@gmail.com>
2023-06-06 19:31:17 +0100

    Widget setup

--- Plugins/OnlineSessions/Source/OnlineSessions/Private/MenuSystem.cpp
+++ Plugins/OnlineSessions/Source/OnlineSessions/Private/MenuSystem.cpp
@@ -3,8 +3,24 @@
 
 #include "MenuSystem.h"
 
+
 void UMenuSystem::MenuSetup()
 {
 	AddToViewport();
 	SetVisibility(ESlateVisibility::Visible);
+	bIsFocusable = true;
+
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		APlayerController* PlayerController = World->GetFirstPlayerController();
+		if (PlayerController)
+		{
+			FInputModeUIOnly InputModeData;
+			InputModeData.SetWidgetToFocus(TakeWidget());
+			InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock); // DoNotLock is default
+			PlayerController->SetInputMode(InputModeData);
+			PlayerController->SetShowMouseCursor(true);
+		}
+	}
 }
