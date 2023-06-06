83e88541c381e22a290884bf42c268dc4ed7f563
JA-Richardson <arichardson1103@gmail.com>
2023-06-06 18:27:25 +0100

    Wiget setup

---
+++ Plugins/OnlineSessions/Source/OnlineSessions/Private/MenuSystem.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MenuSystem.h"
+
+void UMenuSystem::MenuSetup()
+{
+	AddToViewport();
+	SetVisibility(ESlateVisibility::Visible);
+}
---
+++ Plugins/OnlineSessions/Source/OnlineSessions/Public/MenuSystem.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MenuSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONLINESESSIONS_API UMenuSystem : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+		void MenuSetup();
+	
+};
